9b09fa4db4252339aa07af8da90c5adacdee9b4e
kdrbipul <kdrbipul@gmail.com>
2026-03-12 22:56:08 +0600

    provider add

--- lib/data/model/song_model.dart
+++ lib/data/model/song_model.dart
@@ -1,17 +1,33 @@
 import 'package:ostad_14_xylophone_app_media_player/domain/entities/song.dart';
 
-class SongModel extends Song{
- const SongModel({
+class SongModel extends Song {
+  const SongModel({
     required super.title,
     required super.artist,
     required super.url,
-    required super.durationSecond
+    required super.durationSecond,
   });
 
-  static List<Song> getSampleSong(){
+  static List<Song> getSampleSong() {
     return const [
-      SongModel(title: title, artist: artist, url: url, durationSecond: durationSecond),
+      SongModel(
+        title: "Song One",
+        artist: "Artist A",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+        durationSecond: 120,
+      ),
+      SongModel(
+        title: "Song Two",
+        artist: "Artist B",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+        durationSecond: 150,
+      ),
+      SongModel(
+        title: "Song Three",
+        artist: "Artist C",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+        durationSecond: 180,
+      ),
     ];
   }
-
 }
---
+++ lib/presentation/screens/home/provider/media_provider.dart
@@ -0,0 +1,35 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:ostad_14_xylophone_app_media_player/domain/entities/song.dart';
+import '../../../../data/model/song_model.dart';
+
+class MediaProvider extends ChangeNotifier{
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  final List<Song> _playlist = SongModel.getSampleSong();
+
+  int _currentIndex = 0;
+  bool _isPlaying = false;
+  Duration _duration = Duration.zero;
+  Duration _position = Duration.zero;
+
+
+
+  AudioPlayer get audioPlayer => _audioPlayer;
+  List<Song> get playlist => _playlist;
+  Song? get currentSong => _playlist.isNotEmpty ? _playlist[currentIndex] : null;
+  int get currentIndex => _currentIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get duration => _duration;
+  Duration get position => _position;
+
+  MediaProvider (){
+    _initialAudioPlayer();
+  }
+
+  void _initialAudioPlayer() {
+    _audioPlayer.onDurationChanged.listen((newDuration){
+      _duration = newDuration;
+      notifyListeners();
+    });
+  }
+}
